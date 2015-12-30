e581983192c6356250f5bd3ba0a2cc9a779594ce
alexei <alexei@chaptermedia.com>
2015-12-30 01:46:19 +0000

    revert to dumber aggregations

--- lib/tire/search/aggregation.rb
+++ lib/tire/search/aggregation.rb
@@ -7,43 +7,43 @@ module Tire
     #
 
     # Dumber but simpler
-    # class Aggregation
-
-    #   def initialize(name, *options)
-    #     value = if options.size < 2
-    #       options.first || {}
-    #     else
-    #       options
-    #     end
-    #     @hash = { name => value }
-    #   end
-
-    #   def to_json(options={})
-    #     to_hash.to_json
-    #   end
-
-    #   def to_hash
-    #     @hash
-    #   end
-    # end
-
-    # Smarter but more configuration knowledge required
     class Aggregation
 
-      def initialize(name, type, body, &block)
-        @name    = name
-        @value = {type => body}
-        block.arity < 1 ? self.instance_eval(&block) : block.call(self) if block_given?
+      def initialize(name, *options)
+        value = if options.size < 2
+          options.first || {}
+        else
+          options
+        end
+        @hash = { name => value }
       end
 
-      def aggregate(name, type, body={}, &block)
-        @value['aggregations'] ||= {}
-        @value['aggregations'].update Aggregation.new(name, type, body, &block).to_hash
+      def to_json(options={})
+        to_hash.to_json
       end
 
       def to_hash
-        {@name => @value}
+        @hash
       end
     end
+
+    # Smarter but more configuration knowledge required
+    # class Aggregation
+
+    #   def initialize(name, type, body, &block)
+    #     @name    = name
+    #     @value = {type => body}
+    #     block.arity < 1 ? self.instance_eval(&block) : block.call(self) if block_given?
+    #   end
+
+    #   def aggregate(name, type, body={}, &block)
+    #     @value['aggregations'] ||= {}
+    #     @value['aggregations'].update Aggregation.new(name, type, body, &block).to_hash
+    #   end
+
+    #   def to_hash
+    #     {@name => @value}
+    #   end
+    # end
   end
 end
